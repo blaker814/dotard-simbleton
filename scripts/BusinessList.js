e4bed0c59f50adb275005cd1fd07c067b5789518
Ricky Blake <rab0196@yahoo.com>
2020-08-20 08:23:04 -0500

    mapped the agents

--- scripts/BusinessList.js
+++ scripts/BusinessList.js
@@ -1,4 +1,6 @@
-import { allBusinesses } from "./BusinessProvider.js"
+import { allBusinesses, purchaserNames } from "./BusinessProvider.js"
+import { newYorkBusinesses } from "./BusinessProvider.js"
+import { manufacturingBusinesses } from "./BusinessProvider.js"
 import { Company } from "./Business.js"
 
 const contentTarget = document.querySelector("main")
@@ -14,3 +16,43 @@ export const BusinessList = () => {
     );
 }
 
+export const NewYorkBusinessList = () => {
+    const newYorkArray = newYorkBusinesses;
+    contentTarget.innerHTML = "<h2>New York Businesses</h2>"
+
+    newYorkArray.forEach(
+        (busObj) => {
+            contentTarget.innerHTML += `<article class="businessList--newYork">
+            ${Company(busObj)}
+        </article>`
+        }
+    );
+}
+
+export const ManufacturingBusinessList = () => {
+    const manufacturingArray = manufacturingBusinesses;
+    contentTarget.innerHTML = "<h2>Manufacturing Businesses</h2>"
+
+    manufacturingArray.forEach(
+        (busObj) => {
+            contentTarget.innerHTML += `<article class="businessList--manufacturing">
+            ${Company(busObj)}
+        </article>`
+        }
+    );
+}
+
+export const PurchaserList = () => {
+    const purchaserArray = purchaserNames;
+    contentTarget.innerHTML = "<h2>Purchasing Agents</h2>"
+
+    purchaserArray.forEach(
+        (purchaser) => {
+            contentTarget.innerHTML += `<article class="agents">
+            <h3>${purchaser.fullName}</h3>
+                <p>${purchaser.company}</p>
+                <p>${purchaser.phoneNumber}</p>
+        </article>`
+        }
+    );
+}
